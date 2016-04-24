7b8d2766ac4f074e93f46d75f1b9b2914bbb0f12
artekem <artur.mlynarski@gmail.com>
2016-04-24 21:22:15 +0200

    ponowny commit, cos poszlo nie tak

--- js/Controllers/registerController.js
+++ js/Controllers/registerController.js
@@ -10,27 +10,19 @@
             alert("Błąd: " + response.statusText);
             console.error(response);
         }
-		$scope.userPassMessage = 'Hasło niezgodne';
-        $scope.userRegisterEmail = "a@a.com";
-        $scope.userRegisterPassword = "12#Qwe";
-		$scope.userRegisterConfPassword  = "12#Qwe";
-		
-		$scope.hideButton = function() {
-			if ($scope.userRegisterPassword != $scope.userRegisterConfPassword) {
-				$scope.hide = 'true';
-			}
-		}
+        $scope.userRegisterEmail = ' ';
+        $scope.userRegisterPassword = ' ';
+		    $scope.userRegisterConfPassword  = ' ';
 
         $scope.register = function () {
              var userdata = { 
-				email: $scope.userRegisterEmail, 
-				password: $scope.userRegisterPassword,
-				confPass: $scope.userRegisterConfPassword };
+				      email: $scope.userRegisterEmail, 
+				      password: $scope.userRegisterPassword};
              tsffService.getToken(userdata).then(onRegisterCompete, onError);
         };
+        $scope.token = $scope.userdata		
     }
-
-    var app = angular.module("ziwgApp");    
+    var app = angular.module("ziwgApp"); 
     app.controller("registerController", ["$scope", "tsffService", "storage", registerController]);
 
 } ());
