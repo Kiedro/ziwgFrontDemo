ef2dd09dd20ae1c3b9b40349e127ac22cda4c78f
Kiedro <kiedro@outlook.com>
2016-05-07 01:35:04 +0200

    Jeżeli użytkownik ma zapisany token, to przy próbie wejścia na stronę logowania jest automatycznie przerzucany na /logged

--- js/Controllers/loginController.js
+++ js/Controllers/loginController.js
@@ -1,30 +1,36 @@
 (function () {
-    
+
     var loginController = function ($scope, tsffService, storage, $location) {
+
+        (function () {
+            if (storage.getItem("token") != null) {
+                $location.path('/logged');
+            }
+        } ());
 
         var onLoginCompete = function (token) {
             storage.saveItem("token", token);
             $location.path('/logged');
-            $('#spinnerDiv').hide(); 
+            $('#spinnerDiv').hide();
         };
 
         var onError = function (response) {
             alert("Błąd: " + response.statusText);
             console.error(response);
-            $('#spinnerDiv').hide(); 
+            $('#spinnerDiv').hide();
         }
 
         $scope.userLoginEmail = "a@a.com";
         $scope.userLoginPassword = ""; //12#Qwe"
 
         $scope.login = function () {
-             $('#spinnerDiv').show(); 
-             var userdata = { email: $scope.userLoginEmail, password: $scope.userLoginPassword };
-             tsffService.getToken(userdata).then(onLoginCompete, onError);
+            $('#spinnerDiv').show();
+            var userdata = { email: $scope.userLoginEmail, password: $scope.userLoginPassword };
+            tsffService.getToken(userdata).then(onLoginCompete, onError);
         };
     }
 
-    var app = angular.module("ziwgApp");    
+    var app = angular.module("ziwgApp");
     app.controller("loginController", ["$scope", "tsffService", "storage", "$location", loginController]);
 
 } ());
