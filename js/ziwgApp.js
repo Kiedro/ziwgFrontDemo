1b32bf4687a3c5fea4f7837785f92c9e33c63928
artekem <artur.mlynarski@gmail.com>
2016-05-21 15:42:45 +0200

    ostylowano kolejne podstrony, oraz naniesiono poprawki w poprzednich

--- js/ziwgApp.js
+++ js/ziwgApp.js
@@ -5,7 +5,7 @@
     app.config(function ($routeProvider) {
         $routeProvider
             .when("/main", {
-                templateUrl: "views/main.html"
+                templateUrl: "views/main.html",
             })
             .when("/login", {
                 templateUrl: "views/login.html",
@@ -18,10 +18,6 @@
             .when("/userTeams", {
                 templateUrl: "views/loggedUser/userOwnedTeams.html",
                 controller: "userOwnedTeamsController"
-            })
-            .when("/logged", {
-                templateUrl: "views/loggedUser/mainPanel.html",
-                controller: "mainController"
             })
             .when("/logged", {
                 templateUrl: "views/loggedUser/mainPanel.html",
